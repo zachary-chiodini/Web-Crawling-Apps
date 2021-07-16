c3ae43563020cecb7613da9ff9c2c82735efe529
Zachary Chiodini <zacharybchiodini@gmail.com>
2021-07-16 15:01:52 -0400

    Basic Craigs Crawling enabled

--- craigs-crawler/main.py
+++ craigs-crawler/main.py
@@ -2,4 +2,5 @@ from craigs_crawler import CraigsCrawler
 
 craigs_crawler = CraigsCrawler()  # Nationwide search takes some time.
 results = craigs_crawler.search_cars_and_trucks('Volvo S80 V8')
+craigs_crawler.close()
 print(results)
