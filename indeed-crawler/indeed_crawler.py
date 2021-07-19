2f8fd5a82ecaf620422b2c3def175b60f410b42b
Zachary Chiodini <zacharybchiodini@gmail.com>
2021-07-18 23:42:02 -0400

    Fixed a few bugs encountered suring search

--- indeed-crawler/indeed_crawler.py
+++ indeed-crawler/indeed_crawler.py
@@ -8,7 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
+from selenium.common.exceptions import (
+    ElementClickInterceptedException, ElementNotInteractableException,
+    StaleElementReferenceException, TimeoutException)
 
 
 class IndeedCrawler:
@@ -19,6 +21,10 @@ class IndeedCrawler:
         self._headless_mode = headless_mode
         self._driver_path = driver_path
         self._main_window = ''
+        self._cache = set()
+        with open('cache.txt', 'a+') as file:
+            for line in file:
+                self._cache.add(line)
 
     def _setup_real_browser(self) -> None:
         kwargs = {
@@ -125,7 +131,7 @@ class IndeedCrawler:
         return None
 
     def search_jobs(self, query: str) -> None:
-        start = 1
+        start = 0
         while True:
             self._browser.get(
                 'https://www.indeed.com/jobs?q={query}&fromage={days}&start={start}'
@@ -134,19 +140,25 @@ class IndeedCrawler:
             mobtk = search('(?<=data-mobtk=").+?(?=")', self._browser.page_source).group()
             soup_list = BeautifulSoup(self._browser.page_source, 'lxml')\
                 .findAll('a', {'data-mobtk': mobtk})
-            start = len(soup_list)
+            start += len(soup_list)
             for tag in soup_list:
                 quick_apply = BeautifulSoup(str(tag), 'lxml')\
                     .find('span', {'class': 'ialbl iaTextBlack'})
                 if not quick_apply:
                     continue
                 job_jk = search('(?<=data-jk=").+?(?=")', str(tag)).group()
+                if job_jk in self._cache:
+                    continue
                 try:
                     self._apply_for_job(job_jk)
-                except (ElementClickInterceptedException, TimeoutException) as e:
+                except (ElementClickInterceptedException,
+                        ElementNotInteractableException,
+                        StaleElementReferenceException,
+                        TimeoutException) as e:
                     print(str(e))
                     WebDriverWait(self._browser, 10)
-                    self._browser.get_screenshot_as_file('{}-ERROR'.format(job_jk))
+                    with open('{}-ERROR.html'.format(job_jk), 'w', encoding='utf-8') as file:
+                        file.write(self._browser.page_source)
                     if self._browser.current_window_handle != self._main_window:
                         self._browser.close()
                         self._browser.switch_to.window(self._main_window)
@@ -160,13 +172,24 @@ class IndeedCrawler:
                     .find('div', {'class': 'companyLocation'})
                 salary = BeautifulSoup(str(result_content), 'lxml')\
                     .find('span', {'class': 'salary-snippet'})
+                if job_title:
+                    job_title = job_title.get_text()
+                if company_name:
+                    company_name = company_name.get_text()
+                if location:
+                    location = location.get_text()
+                if salary:
+                    salary = salary.get_text()
                 result = {
-                    'Title': job_title.get_text(),
-                    'Company': company_name.get_text(),
-                    'Location': location.get_text(),
-                    'Salary': salary.get_text()
+                    'Title': job_title,
+                    'Company': company_name,
+                    'Location': location,
+                    'Salary': salary
                     }
                 self.results[job_jk] = result
+                self._cache.add(job_jk)
+                with open('cache.txt', 'a') as file:
+                    file.write(job_jk + '\n')
 
     def close(self) -> None:
         self._browser.quit()
--- indeed-crawler/main.py
+++ indeed-crawler/main.py
@@ -3,6 +3,8 @@ import traceback
 
 from indeed_crawler import IndeedCrawler
 
+DEBUG = True
+
 indeed_crawler = IndeedCrawler()
 indeed_crawler.setup_browser()
 indeed_crawler.login(
@@ -12,8 +14,10 @@ indeed_crawler.login(
 try:
     indeed_crawler.search_jobs('data')
 except Exception as e:
+    if DEBUG:
+        traceback.print_exc()
     print(str(e))
 finally:
     results = indeed_crawler.results
-    print('\n\nApplied to {} jobs.'.format(len(results)), end='\n\n')
     pprint(results, width=4)
+    print('\n\nApplied to {} jobs.'.format(len(results)), end='\n\n')
