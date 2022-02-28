ecb2993feaa28db13b2c816c0ef3a6fc397b4de4
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-02-27 21:51:47 -0500

    new features

--- indeed-crawler/main.py
+++ indeed-crawler/main.py
@@ -1,47 +1,33 @@
 from json import load
+from tkinter import StringVar, Tk
 
-# Personal Info
-full_name = ''
-phone = ''
-email = ''
-address = ''
-city = ''
-state = ''
-postal = ''
-country = ''
-linkedin = ''
-website = ''
+COOL_ROBOT_EMOJI = 'icon/cool_robot_emoji.ico'
+COOL_GLASSES_EMOJI = 'icon/cool_glasses.png'
 
-# Education/Skills
-skills = []
-education = ''
-certifications = []
-clearance = ''
-language = []
 
-# Employment/Salary
-salary = ''
-pay_type = ''  # Hourly/Salary
-currency = ''
-employment_type = ''  # Full-time, Part-time
-overtime = ''
-weekends = ''
-hours = ''
+class App:
+    
+    def __init__(self, root_window_: Tk):
+        self._root_window = root_window_
+        self._user_input = {}
+        self._private = {}
+        self._assign_instance_vars()
 
-# Availability
-start_date = ''
-interview_date = ''
-
-# Travel
-licence = ''
-transportation = ''
-travel = ''
-
-# Personal
-of_legal_age = ''
-sponsorship = ''
-eligibility = ''
+    def _assign_instance_vars(self) -> None:
+        with open('default_q_and_a.json') as json:
+            q_and_a = load(json)
+        self._private = q_and_a['Private']
+        del q_and_a['Private']
+        for question in q_and_a:
+            self._user_input[question] = StringVar()
+        return None
 
 
 if __name__ == '__main__':
-    pass
+    root_window = Tk()
+    root_window.geometry('')
+    # root_window.resizable(True, True)
+    root_window.title('Indeed Crawler')
+    root_window.iconbitmap(COOL_ROBOT_EMOJI)
+    App(root_window)
+    root_window.mainloop()
