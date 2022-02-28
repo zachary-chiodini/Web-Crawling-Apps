f5bc3cdfef33202bdd272099a0a5afff82c33efb
Zachary Chiodini <zacharybchiodini@gmail.com>
2022-02-27 19:30:12 -0500

    new features

--- indeed-crawler/main.py
+++ indeed-crawler/main.py
@@ -1,77 +1,9 @@
-from traceback import print_exc
-from typing import List, Tuple
-
-from pandas import DataFrame
-
-from helper_funs import append_df_to_excel
-from indeed_crawler import IndeedCrawler
-
-TOTAL_NUMBER_OF_JOBS = 100
-DEBUG = True
+from json import load
 
 
-queries: List[str] = [
+FIRST_NAME = ''
 
-]
+if __name__ == '__main__':
+    with open('default_q_and_a.json') as json:
+        q_and_a = load(json)
 
-Location, Country = str, str
-places: List[Tuple[Location, Country]] = [
-
-]
-
-negate_jobs: List[str] = [
-
-]
-
-negate_comps: List[str] = [
-
-]
-
-number_of_jobs = int(TOTAL_NUMBER_OF_JOBS/(len(queries) * len(places)))
-
-while (number_of_jobs * len(queries) * len(places)) < TOTAL_NUMBER_OF_JOBS:
-    number_of_jobs += 1
-
-indeed_crawler = IndeedCrawler(
-    number_of_jobs=number_of_jobs,
-    debug=False,
-    manually_fill_out_questions=False
-    )
-
-indeed_crawler.setup_browser()
-indeed_crawler.login(
-    email='',
-    password=''
-    )
-
-abort = False
-for region, country in places:
-    for query in queries:
-        try:
-            indeed_crawler.search_jobs(
-                query=query,
-                job_title_negate_lst=negate_jobs,
-                company_name_negate_lst=negate_comps,
-                past_14_days=False,
-                job_type='',  # fulltime
-                min_salary='',
-                enforce_salary=False,  # consider only jobs with salary listed
-                exp_lvl='',  # entry_level, mid_level, #senior_level
-                remote='',
-                temp_remote=False,
-                country=country,
-                location=region,
-                radius='',
-                auto_answer_questions=True
-                )
-        except Exception as e:
-            if DEBUG:
-                print_exc()
-            else:
-                print(str(e))
-        finally:
-            dataframe = DataFrame(data=indeed_crawler.results)
-            if not dataframe.empty:
-                append_df_to_excel(dataframe, 'submissions.xlsx',
-                                   sheet_name='jobs', index=False)
-                indeed_crawler.reset_results()
