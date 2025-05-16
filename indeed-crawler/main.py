bd51778e7b9fddf8838b262452ec4ba631a72518
Voyager <towgpowerplayer@gmail.com>
2025-05-16 13:13:19 -0400

    alphabetized class methods

--- indeed-crawler/main.py
+++ indeed-crawler/main.py
@@ -90,13 +90,13 @@ class App:
             with open(self._save_file2) as f:
                 for field, value in load(f).items():
                     self._user_input[field]['Variable'].set(value)
-        self._log_box: Text
+        self.log_box: Text
         self._root_frame = Frame(root_window_)
         self._root_frame.pack(expand=True)
-        self._start_button = Button(self._root_frame, command=self._start_crawling)
-        self._user_form(0, 0, 13, 7, 25, 'nesw')
+        self._start_button = Button(self._root_frame, command=self.start_crawling)
+        self.user_form(0, 0, 13, 7, 25, 'nesw')
 
-    def _entry_box(
+    def entry_box(
             self, root_frame: Frame, default_text: str, width: int, row: int, col: int,
             padx: int, pady: int, sticky: str, colspan=1, regex = '', secure=False, value='') -> Entry:
         def bind_clear_default(*args) -> None:
@@ -144,44 +144,7 @@ class App:
             entry.config({'background': 'yellow'})
         return entry
 
-    def _add_entry(self, root_frame: Frame, field: str, width: int,
-            row: int, col: int, padx: int, pady: int, sticky: str, regex: List[str]) -> None:
-        # Detects and processes saved user input.
-        if self._start and any(self._widget_entries[field].values()):
-            for i, (label, entry_list) in enumerate(self._widget_entries[field].items()):
-                for j, value in enumerate(entry_list.copy()):
-                    entry = self._entry_box(root_frame, f"{label} {j + 1}",
-                        width, row + j, col + i, padx, pady, sticky, regex=regex[i], value=value)
-                    entry_list[j] = entry
-            self._start = False
-        else:
-            for i, (label, entry_list) in enumerate(self._widget_entries[field].items()):
-                entry = self._entry_box(root_frame, f"{label} {len(entry_list) + 1}",
-                    width, row + len(entry_list), col + i, padx, pady, sticky, regex=regex[i])
-                entry_list.append(entry)
-        return None
-
-    def _remove_entry(self, field: str) -> None:
-        for label, list_ in self._widget_entries[field].items():
-            if len(list_) > 1:
-                del self._user_input[f"{label} {len(list_)}"]
-                list_.pop(-1).destroy()
-        return None
-
-    def _widget(self, root_frame: Frame, field: str, width: int,
-            row: int, col: int, padx: int, pady: int, sticky: str, regex: List[str]) -> None:
-        add_frame = Frame(root_frame)
-        add_frame.grid(row=row, column=col, padx=padx, pady=pady, sticky=sticky)
-        Label(add_frame, text=field).grid(row=0, column=0, sticky='w')
-        Button(add_frame, text='+', width=2,
-            command=lambda *args: self._add_entry(root_frame, field, width, row + 1, col,
-                padx, pady, sticky, regex)).grid(row=0, column=1, sticky='e')
-        Button(add_frame, text='-', width=2,
-            command=lambda *args: self._remove_entry(field)).grid(row=0, column=2, sticky='e')
-        self._add_entry(root_frame, field, width, row + 1, col, padx, pady, sticky, regex)
-        return None
-
-    def _option_menu(self, root_frame: Frame, field: str,
+    def option_menu(self, root_frame: Frame, field: str,
             row: int, col: int, padx: int, pady: int, sticky: str) -> None:
         def on_select(value: str) -> None:
             selected_var.set(value)
@@ -193,104 +156,20 @@ class App:
             command=on_select).grid(row=row, column=col, padx=padx, pady=pady, sticky='w')
         return None
 
-    def _user_form(self, row: int, col: int, padx: int, pady: int, width: int, sticky) -> None:
-        add_frame = Frame(self._root_frame)
-        add_frame.grid(row=row, column=col, padx=padx, pady=pady, sticky=sticky)
-        Label(add_frame, text='Requirements Document').grid(row=0, column=0, sticky='w')
-        no_rows = 8
-        for i, field in enumerate(self._user_input.copy()):
-            col_i, row_i = i // no_rows, i % no_rows + 1
-            if self._user_input[field]['Entity'] is Entry:
-                self._entry_box(add_frame, field, width, row_i, col_i, padx, pady, sticky,
-                    regex=self._user_input[field]['Regex'], secure=field == 'Indeed Password')
-            elif self._user_input[field]['Entity'] is Button:
-                self._widget(add_frame, field, width, row_i, col_i, padx, pady, sticky,
-                    regex=self._user_input[field]['Regex'])
-            elif self._user_input[field]['Entity'] is Checkbutton:
-                Checkbutton(add_frame, text=field, variable=self._user_input[field]['Variable'])\
-                .grid(row=row_i, column=col_i, sticky='w', padx=padx, pady=pady)
-            elif type(self._user_input[field]['Entity']) is list:
-                self._option_menu(add_frame, field, row_i, col_i, padx, pady, sticky)
-        self._setup_log_box(row + 2, col, padx, 0, width, col_i + 1)
-        image = ImageTk.PhotoImage(Image.open('icon/cool_glasses.png'))
-        self._start_button.image = image
-        self._start_button.configure(image=image)
-        self._start_button.grid(row=row + 3, column=col, sticky='e', padx=padx, pady=pady)
-        add_frame = Frame(self._root_frame)
-        add_frame.grid(row=row + 3, column=col, padx=padx, pady=pady, sticky='w')
-        Button(add_frame, text='Reset Cache', command=self._reset_cache).grid(row=0, column=0, padx=(0, padx))
-        Button(add_frame, text='Save Input', command=self._save_user_input).grid(row=0, column=1, padx=(0, padx))
-        Button(add_frame, text='Clear Input', command=self._clear_user_input).grid(row=0, column=2)
-        self._toggle_start_button()
-        return None
-
-    def _save_user_input(self) -> None:
-        user_input = self._user_input.copy()
-        save_input = {}
-        for field, dict_ in self._widget_entries.items():
-            label_dict = {}
-            for i, (label, entry_list) in enumerate(dict_.items()):
-                label_dict[label] = []
-                for j in range(len(entry_list)):
-                    val_label = f"{label} {j + 1}"
-                    value = user_input[val_label]['Variable'].get()
-                    if value:
-                        label_dict[label].append(value)
-                    del user_input[val_label]
-            # All labels within a widget field must have a value.
-            if len(label_dict) == i + 1:
-                save_input[field] = label_dict
-        with open(path.join(path.dirname(__file__), self._save_file), 'w') as f:
-            dump(save_input, f)
-        with open(path.join(path.dirname(__file__), self._save_file2), 'w') as f:
-            dump({field: dict_['Variable'].get() for field, dict_ in user_input.items()}, f)
-        return None
-
-    def _clear_user_input(self) -> None:
-        for dict_ in self._user_input.values():
-            if dict_['Entity'] is Checkbutton:
-                dict_['Variable'].set(0)
-            else:
-                dict_['Variable'].set('')
-        for dict_ in self._widget_entries.values():
-            for label, entry_list in dict_.items():
-                n = len(entry_list)
-                for i in range(n - 1, -1):
-                    entry_list[i].destroy()
-                    del self._user_input[f"{label} {i + 1}"]
-                entry_list = entry_list[0]
-                self._user_input[f"{label} {n}"]['Variable'].set('')
-        return None
-
-    def _setup_log_box(self, row: int, col: int, padx: int, pady: int, width: str, colspan: int) -> None:
+    def setup_log_box(self, row: int, col: int, padx: int, pady: int, width: str, colspan: int) -> None:
         add_frame = Frame(self._root_frame)
         add_frame.grid(row=row, column=col, columnspan=colspan, padx=padx, pady=pady, sticky='nsew')
-        self._log_box = Text(add_frame, height=10, width=colspan*width - padx)
+        self.log_box = Text(add_frame, height=10, width=colspan*width - padx)
         Label(add_frame, text='Crawler Log:').grid(row=0, column=0, sticky='w')
-        scroll_bar = Scrollbar(add_frame, command=self._log_box.yview)
-        self._log_box.config(yscrollcommand=scroll_bar.set)
+        scroll_bar = Scrollbar(add_frame, command=self.log_box.yview)
+        self.log_box.config(yscrollcommand=scroll_bar.set)
         scroll_bar.grid(row=1, column=0, sticky='nse')
-        self._log_box.grid(row=1, column=0, sticky='w')
-        self._log_box.insert('end', 'Please fill out the above information. Required input is highlighted.')
-        self._log_box.configure(state='disabled')
+        self.log_box.grid(row=1, column=0, sticky='w')
+        self.log_box.insert('end', 'Please fill out the above information. Required input is highlighted.')
+        self.log_box.configure(state='disabled')
         return None
 
-    def _toggle_start_button(self, *args) -> None:
-        if all(self._user_input[field]['Variable'].get() for field in self._required_input if field in self._user_input)\
-            & all(entry_list[0].get() for entry_list in self._widget_entries['Skills/Experience'].values()):
-            self._start_button.configure(state='normal')
-        else:
-            self._start_button.configure(state='disable')
-        return None
-
-    @staticmethod
-    def _reset_cache(*args) -> None:
-        if path.exists('cache.txt'):
-            with open('cache.txt', 'w') as cache:
-                cache.truncate()
-        return None
-
-    def _start_crawling(self) -> None:
+    def start_crawling(self) -> None:
         """Processes data from user input before crawling."""
         input_q_and_a = {}
         for skill, experience in zip(self._widget_entries['Skills/Experience']['Skill'],
@@ -354,9 +233,130 @@ class App:
                 word.strip() for word in self._user_input['Companies to Avoid (Comma Separated)'].get().split(',')],
             min_salary=self._user_input['Desired Salary'].get(),
             default_q_and_a=input_q_and_a,
-            log_box=self._log_box)
+            log_box=self.log_box)
         new_thread = Thread(target=run_crawler.start)
         new_thread.start()
+        return None
+
+    def widget(self, root_frame: Frame, field: str, width: int,
+            row: int, col: int, padx: int, pady: int, sticky: str, regex: List[str]) -> None:
+        add_frame = Frame(root_frame)
+        add_frame.grid(row=row, column=col, padx=padx, pady=pady, sticky=sticky)
+        Label(add_frame, text=field).grid(row=0, column=0, sticky='w')
+        Button(add_frame, text='+', width=2,
+            command=lambda *args: self._add_entry(root_frame, field, width, row + 1, col,
+                padx, pady, sticky, regex)).grid(row=0, column=1, sticky='e')
+        Button(add_frame, text='-', width=2,
+            command=lambda *args: self._remove_entry(field)).grid(row=0, column=2, sticky='e')
+        self._add_entry(root_frame, field, width, row + 1, col, padx, pady, sticky, regex)
+        return None
+
+    def user_form(self, row: int, col: int, padx: int, pady: int, width: int, sticky) -> None:
+        add_frame = Frame(self._root_frame)
+        add_frame.grid(row=row, column=col, padx=padx, pady=pady, sticky=sticky)
+        Label(add_frame, text='Requirements Document').grid(row=0, column=0, sticky='w')
+        no_rows = 8
+        for i, field in enumerate(self._user_input.copy()):
+            col_i, row_i = i // no_rows, i % no_rows + 1
+            if self._user_input[field]['Entity'] is Entry:
+                self.entry_box(add_frame, field, width, row_i, col_i, padx, pady, sticky,
+                    regex=self._user_input[field]['Regex'], secure=field == 'Indeed Password')
+            elif self._user_input[field]['Entity'] is Button:
+                self.widget(add_frame, field, width, row_i, col_i, padx, pady, sticky,
+                    regex=self._user_input[field]['Regex'])
+            elif self._user_input[field]['Entity'] is Checkbutton:
+                Checkbutton(add_frame, text=field, variable=self._user_input[field]['Variable'])\
+                .grid(row=row_i, column=col_i, sticky='w', padx=padx, pady=pady)
+            elif type(self._user_input[field]['Entity']) is list:
+                self.option_menu(add_frame, field, row_i, col_i, padx, pady, sticky)
+        self.setup_log_box(row + 2, col, padx, 0, width, col_i + 1)
+        image = ImageTk.PhotoImage(Image.open('icon/cool_glasses.png'))
+        self._start_button.image = image
+        self._start_button.configure(image=image)
+        self._start_button.grid(row=row + 3, column=col, sticky='e', padx=padx, pady=pady)
+        add_frame = Frame(self._root_frame)
+        add_frame.grid(row=row + 3, column=col, padx=padx, pady=pady, sticky='w')
+        Button(add_frame, text='Reset Cache', command=self._reset_cache).grid(row=0, column=0, padx=(0, padx))
+        Button(add_frame, text='Save Input', command=self._save_user_input).grid(row=0, column=1, padx=(0, padx))
+        Button(add_frame, text='Clear Input', command=self._clear_user_input).grid(row=0, column=2)
+        self._toggle_start_button()
+        return None
+
+    def _add_entry(self, root_frame: Frame, field: str, width: int,
+            row: int, col: int, padx: int, pady: int, sticky: str, regex: List[str]) -> None:
+        # Detects and processes saved user input.
+        if self._start and any(self._widget_entries[field].values()):
+            for i, (label, entry_list) in enumerate(self._widget_entries[field].items()):
+                for j, value in enumerate(entry_list.copy()):
+                    entry = self.entry_box(root_frame, f"{label} {j + 1}",
+                        width, row + j, col + i, padx, pady, sticky, regex=regex[i], value=value)
+                    entry_list[j] = entry
+            self._start = False
+        else:
+            for i, (label, entry_list) in enumerate(self._widget_entries[field].items()):
+                entry = self.entry_box(root_frame, f"{label} {len(entry_list) + 1}",
+                    width, row + len(entry_list), col + i, padx, pady, sticky, regex=regex[i])
+                entry_list.append(entry)
+        return None
+
+    def _clear_user_input(self) -> None:
+        for dict_ in self._user_input.values():
+            if dict_['Entity'] is Checkbutton:
+                dict_['Variable'].set(0)
+            else:
+                dict_['Variable'].set('')
+        for dict_ in self._widget_entries.values():
+            for label, entry_list in dict_.items():
+                n = len(entry_list)
+                for i in range(n - 1, -1):
+                    entry_list[i].destroy()
+                    del self._user_input[f"{label} {i + 1}"]
+                entry_list = entry_list[0]
+                self._user_input[f"{label} {n}"]['Variable'].set('')
+        return None
+
+    def _remove_entry(self, field: str) -> None:
+        for label, list_ in self._widget_entries[field].items():
+            if len(list_) > 1:
+                del self._user_input[f"{label} {len(list_)}"]
+                list_.pop(-1).destroy()
+        return None
+
+    @staticmethod
+    def _reset_cache(*args) -> None:
+        if path.exists('cache.txt'):
+            with open('cache.txt', 'w') as cache:
+                cache.truncate()
+        return None
+
+    def _save_user_input(self) -> None:
+        user_input = self._user_input.copy()
+        save_input = {}
+        for field, dict_ in self._widget_entries.items():
+            label_dict = {}
+            for i, (label, entry_list) in enumerate(dict_.items()):
+                label_dict[label] = []
+                for j in range(len(entry_list)):
+                    val_label = f"{label} {j + 1}"
+                    value = user_input[val_label]['Variable'].get()
+                    if value:
+                        label_dict[label].append(value)
+                    del user_input[val_label]
+            # All labels within a widget field must have a value.
+            if len(label_dict) == i + 1:
+                save_input[field] = label_dict
+        with open(path.join(path.dirname(__file__), self._save_file), 'w') as f:
+            dump(save_input, f)
+        with open(path.join(path.dirname(__file__), self._save_file2), 'w') as f:
+            dump({field: dict_['Variable'].get() for field, dict_ in user_input.items()}, f)
+        return None
+
+    def _toggle_start_button(self, *args) -> None:
+        if all(self._user_input[field]['Variable'].get() for field in self._required_input if field in self._user_input)\
+            & all(entry_list[0].get() for entry_list in self._widget_entries['Skills/Experience'].values()):
+            self._start_button.configure(state='normal')
+        else:
+            self._start_button.configure(state='disable')
         return None
 
 
